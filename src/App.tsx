455d2d41e4d0d7c0d37f69144df4388c2b93e4a3
nandoProgrammer <nandinhop.costa@gmail.com>
2023-03-19 20:57:36 -0300

    start new site

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+     <div className="content">
+        <h1>Novo Site Em Construção...</h1>
+        <ul>
+           <li>
+              <a href="github">Meu Github</a>
+           </li>
+           <li>
+              <a href="github">Meu Linkedin</a>
+           </li>
+        </ul>
+     </div>
+  );
+}
+
+export default App;
