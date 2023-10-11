24034f41ba1ca77151877e1dd0e00c3434705e5f
nandoProgrammer <nandinhop.costa@gmail.com>
2023-10-11 07:45:52 -0300

    feat: criação do componente header

---
+++ src/components/Header/Header.style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Logo = styled.h1`
+    color: ${(props) => props.theme.color.primary};
+    font-size: 32px;
+    font-weight: 300;
+`;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import  { Logo } from './Header.style';
+
+export const Header = () => {
+    return <Logo>Fernando Costa</Logo>
+}
+
+export default Header;
