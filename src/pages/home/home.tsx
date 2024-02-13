64882095b044e2837c245f1c893d4ed43659d6e7
Fernando Costa <62177135+nandoProgrammer@users.noreply.github.com>
2024-02-13 15:24:01 -0300

    hotfix: ajustes build

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import { WrapperHome } from './Home.style';
 import { Skills } from '../../components/Skills/Skills';
-import { Projects } from '../../components/Projects/Projects';
 import { AboutMe } from '../../components/AboutMe/AboutMe';
 
 import { Wrapper, Separator } from '../../styles/bootstrap';
