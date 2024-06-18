49faac0fc5fb69fcd437e9115f60d5c9113bd1e8
Fernando Costa <62177135+nandoProgrammer@users.noreply.github.com>
2024-06-18 09:24:10 -0300

    feat: ajustes header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,34 +1,49 @@
 import React from "react";
-import  { HeaderTop } from './Header.style';
+import { HeaderTop } from "./Header.style";
 import { Nav } from "../../styles/bootstrap";
-import { Wrapper, HeadingMd } from '../../styles/bootstrap';
+import { Wrapper, HeadingMd } from "../../styles/bootstrap";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedinIn, faGithub, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLinkedinIn,
+  faGithub,
+  faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
 
-const redirectTo = (url: string) => setTimeout(() => {
-    window.open(url, '_blank');
-}, 1000);
+const redirectTo = (url: string) =>
+  setTimeout(() => {
+    window.open(url, "_blank");
+  }, 1000);
 
 export const Header: React.FunctionComponent = () => {
-    return (
-        <HeaderTop>
-            <Wrapper direction={'row'} justify={'space-between'} align={'center'}>
-                <HeadingMd>fernando.costa</HeadingMd>
-                <Nav>
-                    <ul>
-                        <li onClick={() => redirectTo('https://www.linkedin.com/in/fernando-pereira-costa')}> 
-                          <FontAwesomeIcon icon={faLinkedinIn} />
-                        </li>
-                        <li onClick={() => redirectTo('https://github.com/nandoProgrammer')}>
-                           <FontAwesomeIcon icon={faGithub} />
-                        </li>
-                        <li onClick={() => redirectTo('https://wa.me/5587999289850')}>
-                           <FontAwesomeIcon icon={faWhatsapp} />
-                        </li>
-                    </ul>
-                </Nav>
-            </Wrapper>
-        </HeaderTop>
-    )
-}
+  return (
+    <HeaderTop>
+      <Wrapper
+        direction={"row"}
+        justify={"space-between"}
+        alignItems={"center"}
+      >
+        <HeadingMd>fernando.costa</HeadingMd>
+        <Nav>
+          <ul>
+            <li
+              onClick={() =>
+                redirectTo("https://www.linkedin.com/in/fernando-pereira-costa")
+              }
+            >
+              <FontAwesomeIcon icon={faLinkedinIn} />
+            </li>
+            <li
+              onClick={() => redirectTo("https://github.com/nandoProgrammer")}
+            >
+              <FontAwesomeIcon icon={faGithub} />
+            </li>
+            <li onClick={() => redirectTo("https://wa.me/5587999289850")}>
+              <FontAwesomeIcon icon={faWhatsapp} />
+            </li>
+          </ul>
+        </Nav>
+      </Wrapper>
+    </HeaderTop>
+  );
+};
