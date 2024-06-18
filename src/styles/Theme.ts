8bf7fbe6675dd770aad6e9f02d3d4ae789b0ed13
Fernando Costa <62177135+nandoProgrammer@users.noreply.github.com>
2024-06-18 09:22:46 -0300

    feat: ajustes nas variáveis do tema

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -1,23 +1,26 @@
 const theme = {
-    colors: {
-        primary: "#151515",
-        secondary: "#4EE1A0",
-        gray: "#242424",
-        cream: "#d9d9d9",
-        white: "#fff",
-        black: "#000"
+  colors: {
+    primary: "#151515",
+    gray: "#242424",
+    cream: "#d9d9d9",
+    white: "#fff",
+    black: "#000",
+  },
+  spacing: {
+    sm: "39px",
+  },
+  font: {
+    family: "Space Grotesk",
+    gradient: {
+      primary: "#3498db",
+      secondary: "#8e44ad",
     },
-    spacing: {
-        sm: "39px"
-    },
-    font: {
-        family: 'Space Grotesk',
-    },
-    breakpoints: {
-        lg: '1440px',
-        md: '768px',
-        sm: '345px'
-    }
+  },
+  breakpoints: {
+    lg: "1440px",
+    md: "768px",
+    sm: "345px",
+  },
 };
-  
+
 export default theme;
